daffcc29548c292fdfffea3094dab12fbeeb712b
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-16 15:51:51 +0300

    Create test to validate endangered animals class instantiates correctly.

---
+++ src/test/java/models/EndangeredAnimalsTest.java
@@ -0,0 +1,14 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EndangeredAnimalsTest {
+
+    @Test
+    public void endangeredAnimals_instantiatesCorrectly_true() {
+        EndangeredAnimals animal = new EndangeredAnimals();
+        assertTrue(true);
+    }
+}
