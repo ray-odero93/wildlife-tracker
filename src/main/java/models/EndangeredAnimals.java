d20961d05b51c21cb7de728916364487a789eae9
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-16 15:54:47 +0300

    Declare properties for endangered animals class objects.

--- src/main/java/models/EndangeredAnimals.java
+++ src/main/java/models/EndangeredAnimals.java
@@ -1,4 +1,11 @@
 package models;
 
 public class EndangeredAnimals {
+    private int id;
+    private String name;
+    private String health;
+    private int age;
+    private boolean endangered;
+
+
 }
