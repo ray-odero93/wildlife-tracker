2ee72201b9fa9fab3af7dc39c5fa269e931eb8ff
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-16 08:44:19 +0300

    Create test to validate given animal name type.

--- src/test/java/models/AnimalsTest.java
+++ src/test/java/models/AnimalsTest.java
@@ -1,6 +1,20 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class AnimalsTest {
-  
+
+    @Test
+    public void animals_instantiatesCorrectly_true() {
+        Animals animal = new Animals("dog", 1);
+        assertTrue(animal instanceof Animals);
+    }
+
+    @Test
+    public void animals_instantiatesWithCorrectName_string() {
+        Animals animal = new Animals("dog", 1);
+        assertEquals("dog", animal.getName());
+    }
 }
