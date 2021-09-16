fa3c8d0d8bfa66bdc47de21bbb7c13fbdc9790d3
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-16 16:09:46 +0300

    Create method to return health attribute of endangered animal.

--- src/main/java/models/EndangeredAnimals.java
+++ src/main/java/models/EndangeredAnimals.java
@@ -4,13 +4,17 @@ public class EndangeredAnimals {
     private int id;
     private String name;
     private String health;
-    private int age;
+    private String age;
     private boolean endangered;
 
-    public EndangeredAnimals(String name, String health, int age) {
+    public EndangeredAnimals(String name, String health, String age) {
         this.name = name;
         this.health = health;
         this.age = age;
         this.id = id;
+    }
+
+    public String getHealth() {
+        return health;
     }
 }
