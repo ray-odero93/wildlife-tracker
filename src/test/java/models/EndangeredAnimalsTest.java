f1f00ef4553e6638cb67a8deca5582f0da90acd4
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-16 16:07:50 +0300

    Create test to validate health attribute.

--- src/test/java/models/EndangeredAnimalsTest.java
+++ src/test/java/models/EndangeredAnimalsTest.java
@@ -8,7 +8,13 @@ public class EndangeredAnimalsTest {
 
     @Test
     public void endangeredAnimals_instantiatesCorrectly_true() {
-        EndangeredAnimals animal = new EndangeredAnimals();
+        EndangeredAnimals animal = new EndangeredAnimals("Jackal", "Healthy", "Young");
         assertTrue(true);
+    }
+
+    @Test
+    public void getHealth_returnsHealthAttribute_true() {
+        EndangeredAnimals testAnimal = new EndangeredAnimals("Jackal", "Healthy", "Young");
+        assertEquals("Healthy", testAnimal.getHealth());
     }
 }
