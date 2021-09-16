ce5a34bc388fcf009fa998979060ff749ef3b98d
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-16 18:08:24 +0300

    Define test to validate creation of Sightings instance.

--- src/test/java/models/SightingsTest.java
+++ src/test/java/models/SightingsTest.java
@@ -1,7 +1,16 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SightingsTest {
 
+    @Test
+    public void sighting_instantiatesCorrectly_true() {
+        Animals animal = new Animals("dog");
+        animal.save();
+        Sightings testSightings = new Sightings(animal.getId(), "Zone D", "John");
+        assertEquals(true, testSightings instanceof Sightings);
+    }
 }
