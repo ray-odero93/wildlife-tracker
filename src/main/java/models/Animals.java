c274d89d57beda58a918d7ba058c5149998c8090
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-17 14:08:18 +0300

    Update deletion method.

--- src/main/java/models/Animals.java
+++ src/main/java/models/Animals.java
@@ -71,8 +71,8 @@ public class Animals {
     public void deleteInstance() {
         try (Connection conn = DB.sql2o.open()) {
             String sql = "DELETE FROM animals WHERE id=:id;";
-            conn.createQuery(sql
-                    .addParameter("id", id))
+            conn.createQuery(sql)
+                    .addParameter("id", id)
                     .executeUpdate();
         }
     }
